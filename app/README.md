ab663e5ef8aef134625ec28f62113e290b00617f
generalsanjeet <generalsanjeet@gmail.com>
2023-11-30 13:56:49 +0530

    frontend added

---
+++ app/README.md
@@ -0,0 +1,4 @@
+# nextjs-dashboard
+just for practice
+
+hello sanjeet
---
+++ app/app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>customers page</p>
+    </div>
+  );
+};
+
+export default Page;
