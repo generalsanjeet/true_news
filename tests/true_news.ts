bdf838b6b3bfc240359699352e06b41118a3aaad
generalsanjeet <generalsanjeet@gmail.com>
2023-12-01 06:30:48 +0530

    initial test case is written

--- tests/true_news.ts
+++ tests/true_news.ts
@@ -3,14 +3,22 @@ import { Program } from "@coral-xyz/anchor";
 import { TrueNews } from "../target/types/true_news";
 
 describe("true_news", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    anchor.setProvider(anchor.AnchorProvider.env());
+    const program = anchor.workspace.TrueNews as Program<TrueNews>;
 
-  const program = anchor.workspace.TrueNews as Program<TrueNews>;
+    it("can publish a new news", async () => {
+        const newsKeyPair = anchor.web3.Keypair.generate();
+        await program.methods.publishNews("sanjeet channel news", "my first news")
+            .accounts({
+                myNews: newsKeyPair.publicKey,
+                publisherOfNews: program.provider.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            })
+            .signers([newsKeyPair])
+            .rpc();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+        const newsAccount = await program.account.news.fetch(newsKeyPair.publicKey);
+        console.log(newsAccount);
+    });
 });
+
